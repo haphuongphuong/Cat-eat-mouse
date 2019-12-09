aecb0315fe4c5748df71ddf65b8ba3b28a7c6fb0
Ha Nguyen Hai Dang <dota.angiang76k@gmail.com>
2019-12-09 18:41:06 +0700

    sayHi methord

--- Dog.js
+++ Dog.js
@@ -1,6 +1,14 @@
-function Dog(){
+var chalk = require('chalk');
+function Dog(name){
     this.stomach = [];
+    this.name = name;
 }
 Dog.prototype.eat = function(Cat){
     this.stomach.push(Cat);
 }
+
+Dog.prototype.sayHi = function() {
+	console.log("Hi! I am a dog. My name is " + chalk.red(this.name));
+};
+
+module.exports = Dog;
